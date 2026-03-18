8f62c5c18f59a3b6baf026def68e34e2f42ecf8e
mrprince998 <peincerana234@gmail.com>
2026-03-18 16:13:33 +0545

    fix: database config

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -8,17 +8,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        // entities: [],
-        autoLoadEntities: true,
-        synchronize: true, // development only
-      }),
+      useFactory: (configService: ConfigService) => {
+        const databasePublicUrl = configService.get<string>('DATABASE_PUBLIC_URL');
+
+        return {
+          type: 'postgres',
+          ...(databasePublicUrl
+            ? { url: databasePublicUrl }
+            : {
+                host: configService.get<string>('DB_HOST'),
+                port: configService.get<number>('DB_PORT'),
+                username: configService.get<string>('DB_USERNAME'),
+                password: configService.get<string>('DB_PASSWORD'),
+                database: configService.get<string>('DB_NAME'),
+              }),
+          // entities: [],
+          autoLoadEntities: true,
+          synchronize: true, // development only
+        };
+      },
     }),
   ],
 })
